24ad67074fb8a1aff9fde33b7d093f41f38ee780
vitaminac <dalao1002@gmail.com>
2024-10-26 20:24:32 +0000

    automatic gh pages update

--- _next/static/fixed/_buildManifest.js
+++ _next/static/fixed/_buildManifest.js
@@ -1,36 +1,34 @@
 (self.__BUILD_MANIFEST = (function (s, a) {
   return {
     __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
-    "/": ["static/chunks/pages/index-7537aa0f8e48c0c6.js"],
-    "/404": ["static/chunks/pages/404-92e994caa6e0fb60.js"],
-    "/_error": ["static/chunks/pages/_error-7a92967bea80186d.js"],
+    "/": ["static/chunks/pages/index.js"],
+    "/404": ["static/chunks/pages/404.js"],
+    "/_error": ["static/chunks/pages/_error.js"],
     "/archives/[slug]": [
-      "static/chunks/5423ad26-257b22e576552beb.js",
-      "static/chunks/175675d1-4ecee8fd0a8f0622.js",
-      "static/chunks/893-04920b8a1103a842.js",
-      "static/chunks/pages/archives/[slug]-06db4819515184e6.js",
+      "static/chunks/5423ad26.js",
+      "static/chunks/175675d1.js",
+      "static/chunks/893.js",
+      "static/chunks/pages/archives/[slug].js",
     ],
-    "/posts/langs/[lang]": [
-      "static/chunks/pages/posts/langs/[lang]-5e2e3629b1d421c7.js",
-    ],
+    "/posts/langs/[lang]": ["static/chunks/pages/posts/langs/[lang].js"],
     "/posts/langs/[lang]/pagination/[pagination]": [
       s,
       a,
-      "static/chunks/pages/posts/langs/[lang]/pagination/[pagination]-0aaba5ef414795c2.js",
+      "static/chunks/pages/posts/langs/[lang]/pagination/[pagination].js",
     ],
     "/posts/langs/[lang]/tags": [
       a,
-      "static/chunks/pages/posts/langs/[lang]/tags-ea8d4311530b97b0.js",
+      "static/chunks/pages/posts/langs/[lang]/tags.js",
     ],
     "/posts/langs/[lang]/tags/[tag]": [
       s,
       a,
-      "static/chunks/pages/posts/langs/[lang]/tags/[tag]-d3e9f6b8d97c42d3.js",
+      "static/chunks/pages/posts/langs/[lang]/tags/[tag].js",
     ],
     "/posts/[slug]": [
       s,
-      "static/chunks/287-15331c4732f63f37.js",
-      "static/chunks/pages/posts/[slug]-334a627f6bb72763.js",
+      "static/chunks/287.js",
+      "static/chunks/pages/posts/[slug].js",
     ],
     sortedPages: [
       "/",
@@ -45,5 +43,5 @@
       "/posts/[slug]",
     ],
   };
-})("static/chunks/470-88ca63604181d961.js", "static/css/ef9f9e2030bad8a8.css")),
+})("static/chunks/470.js", "static/css/ef9f9e2030bad8a8.css")),
   self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
