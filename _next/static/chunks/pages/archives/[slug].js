c14fc376009ca9b7692ea6e766d0b62d45db8934
vitaminac <dalao1002@gmail.com>
2024-10-28 21:34:03 +0000

    automatic gh pages update

--- _next/static/chunks/pages/archives/[slug].js
+++ _next/static/chunks/pages/archives/[slug].js
@@ -1,7 +1,7 @@
 (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
   [874],
   {
-    4384: function (A, e, t) {
+    4384: function (e, A, t) {
       (window.__NEXT_P = window.__NEXT_P || []).push([
         "/archives/[slug]",
         function () {
@@ -9,10 +9,10 @@
         },
       ]);
     },
-    2610: function (A, e, t) {
+    2610: function (e, A, t) {
       "use strict";
-      t.r(e),
-        (e.default = {
+      t.r(A),
+        (A.default = {
           src: "/_next/static/media/test.bd907638.jpg",
           height: 1920,
           width: 1920,
@@ -22,34 +22,36 @@
           blurHeight: 8,
         });
     },
-    4907: function (A, e, t) {
+    4907: function (e, A, t) {
       "use strict";
-      t.r(e),
-        t.d(e, {
+      t.r(A),
+        t.d(A, {
           __N_SSG: function () {
-            return b;
+            return _;
           },
           default: function () {
-            return C;
+            return b;
           },
         });
       var r = t(5893),
         n = t(7294),
         i = t(9875),
         s = t(3698),
-        u = t(65),
-        a = t(7437),
-        o = t(5343),
-        c = t(3846),
-        l = t(1623);
-      let h = () => (A) => {
-        (0, l.Vn)(A, (A) => {
-          if ("leafDirective" === A.type && "youtube" === A.name) {
-            let e = A.data || (A.data = {});
-            (e.hName = "iframe"),
-              (e.hProperties = {
-                src: "https://www.youtube.com/embed/".concat(A.attributes.vid),
-                title: A.children[0].value,
+        u = t(3219),
+        a = t(259),
+        o = t(7423),
+        c = t(7437),
+        l = t(5343),
+        h = t(8848),
+        d = t(1623);
+      let g = () => (e) => {
+        (0, d.Vn)(e, (e) => {
+          if ("leafDirective" === e.type && "youtube" === e.name) {
+            let A = e.data || (e.data = {});
+            (A.hName = "iframe"),
+              (A.hProperties = {
+                src: "https://www.youtube.com/embed/".concat(e.attributes.vid),
+                title: e.children[0].value,
                 width: "560",
                 height: "315",
                 frameborder: "0",
@@ -57,31 +59,50 @@
                   "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
                 allowfullscreen: !0,
               }),
-              (A.children = []);
+              (e.children = []);
           }
         });
       };
-      var g = t(7797),
-        d = t(5280),
-        f = t(9360),
-        p = t(1924),
-        E = t(2935),
-        w = t(3687),
-        m = t(261);
-      let Q = {
-        img: function (A) {
-          let { alt: e, src: n, width: i, height: s, ...u } = A;
+      var p = t(9685),
+        f = t(5280),
+        E = t(9360),
+        w = t(6702),
+        m = t(2935),
+        v = t(2609);
+      let Q = "TOC GO HERE";
+      function x() {
+        return (e) => {
+          var A;
+          let t = e.children.findIndex(
+            (e) =>
+              "heading" === e.type &&
+              2 === e.depth &&
+              "text" === e.children[0].type &&
+              e.children[0].value === Q,
+          );
+          -1 !== t &&
+          (null === (A = e.children[t + 1]) || void 0 === A
+            ? void 0
+            : A.type) === "list"
+            ? (e.children = [e.children[t + 1]])
+            : (e.children = []);
+        };
+      }
+      var y = t(261);
+      let B = {
+        img: function (e) {
+          let { alt: A, src: n, width: i, height: s, ...u } = e;
           return n.startsWith("http")
             ? (0, r.jsx)("img", {
                 src: n,
-                alt: e,
+                alt: A,
                 width: i && (Number.isInteger(i) ? i : parseInt(i)),
                 height: s && (Number.isInteger(s) ? s : parseInt(s)),
                 ...u,
               })
-            : (0, r.jsx)(m.t, {
+            : (0, r.jsx)(y.t, {
                 src: t(7192)("./".concat(n)),
-                alt: e || "image",
+                alt: A || "image",
                 sizes: "responsive",
                 width: void 0,
                 height: void 0,
@@ -89,28 +110,28 @@
               });
         },
       };
-      function B(A) {
-        let { markdownSource: e } = A;
+      function I(e) {
+        let { markdownSource: A } = e;
         return (0, n.useMemo)(
           () =>
-            (function (A, e) {
+            (function (e, A) {
               let t = {
                 jsx: r.jsx,
                 jsxs: r.jsxs,
                 Fragment: r.Fragment,
-                components: e,
+                components: A,
                 development: !1,
               };
               return (0, i.l)()
                 .use(s.Z)
-                .use([u.Z, a.Z, o.Z, c.Z, h])
-                .use(g.Z)
+                .use([o.Z, c.Z, l.Z, h.Z, g])
+                .use(p.Z)
                 .use([
-                  d.Z,
                   f.Z,
-                  p.Z,
+                  E.Z,
+                  w.Z,
                   [
-                    E.Z,
+                    m.Z,
                     {
                       behavior: "prepend",
                       content: {
@@ -122,42 +143,63 @@
                     },
                   ],
                 ])
-                .use(w.Z, t)
-                .processSync(A).result;
-            })(e, Q),
-          [e],
+                .use(v.Z, t)
+                .processSync(e).result;
+            })(A, B),
+          [A],
         );
       }
-      var b = !0,
-        C = function (A) {
-          let { markdownContentSource: e } = A;
-          return (0, r.jsx)(B, { markdownSource: e });
+      var _ = !0,
+        b = function (e) {
+          let { markdownContentSource: A } = e,
+            t = (0, n.useMemo)(
+              () =>
+                (function (e) {
+                  let A = (0, i.l)()
+                    .use(s.Z)
+                    .use(a.Z, { heading: Q, maxDepth: 3 })
+                    .use(x)
+                    .use(u.Z)
+                    .processSync("## ".concat(Q, "\n") + e);
+                  if ("string" != typeof A.value) throw Error("wrong type");
+                  return A.value;
+                })(A),
+              [A],
+            );
+          return (0, r.jsxs)("main", {
+            children: [
+              (0, r.jsx)("nav", { dangerouslySetInnerHTML: { __html: t } }),
+              (0, r.jsx)("article", {
+                children: (0, r.jsx)(I, { markdownSource: A }),
+              }),
+            ],
+          });
         };
     },
-    7192: function (A, e, t) {
+    7192: function (e, A, t) {
       var r = { "./test.jpg": 2610 };
-      function n(A) {
-        return t(i(A));
+      function n(e) {
+        return t(i(e));
       }
-      function i(A) {
-        if (!t.o(r, A)) {
-          var e = Error("Cannot find module '" + A + "'");
-          throw ((e.code = "MODULE_NOT_FOUND"), e);
+      function i(e) {
+        if (!t.o(r, e)) {
+          var A = Error("Cannot find module '" + e + "'");
+          throw ((A.code = "MODULE_NOT_FOUND"), A);
         }
-        return r[A];
+        return r[e];
       }
       (n.keys = function () {
         return Object.keys(r);
       }),
         (n.resolve = i),
-        (A.exports = n),
+        (e.exports = n),
         (n.id = 7192);
     },
   },
-  function (A) {
-    A.O(0, [658, 265, 893, 888, 774, 179], function () {
-      return A((A.s = 4384));
+  function (e) {
+    e.O(0, [658, 265, 321, 888, 774, 179], function () {
+      return e((e.s = 4384));
     }),
-      (_N_E = A.O());
+      (_N_E = e.O());
   },
 ]);
