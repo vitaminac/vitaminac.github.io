fd0f419d6c8b2e8b64ce501c37fec1c87a0e0e9e
vitaminac <dalao1002@gmail.com>
2026-08-21 22:32:53 +0000

    automatic gh pages update

--- _next/static/fixed/_buildManifest.js
+++ _next/static/fixed/_buildManifest.js
@@ -1,11 +1,14 @@
-(self.__BUILD_MANIFEST = (function (s, a, t, n, g) {
+(self.__BUILD_MANIFEST = (function (s, a, t, n) {
   return {
     __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
     "/": ["static/chunks/pages/index.js"],
     "/404": ["static/chunks/pages/404.js"],
     "/_error": ["static/chunks/pages/_error.js"],
-    "/flash/langs/[lang]": [s, g, "static/chunks/pages/flash/langs/[lang].js"],
-    "/flash/[id]": [s, g, "static/chunks/pages/flash/[id].js"],
+    "/flash/[id]": [
+      s,
+      "static/css/ef1dc90026d69db2.css",
+      "static/chunks/pages/flash/[id].js",
+    ],
     "/posts/langs/[lang]": ["static/chunks/pages/posts/langs/[lang].js"],
     "/posts/langs/[lang]/pagination/[pagination]": [
       s,
@@ -39,7 +42,6 @@
       "/404",
       "/_app",
       "/_error",
-      "/flash/langs/[lang]",
       "/flash/[id]",
       "/posts/langs/[lang]",
       "/posts/langs/[lang]/pagination/[pagination]",
@@ -53,6 +55,5 @@
   "static/chunks/503.js",
   "static/chunks/151.js",
   "static/css/b6ae49f768350e65.css",
-  "static/css/ef1dc90026d69db2.css",
 )),
   self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
